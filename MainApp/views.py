f77a4d849fe0fdb75b555af4f60d98065340aab2
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-14 14:17:50 -0500

    views, templates created

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # The home page for Learning Log
-    return render(request, "mainapp/index.html")
+    return render(request, "MainApp/index.html")
