a73b1c15400f6365e5464b6e3877b7bba4702032
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-14 13:50:14 -0500

    entries output

--- MyShell.py
+++ MyShell.py
@@ -6,14 +6,19 @@ import django
 
 django.setup()
 
-from MainApp.models import Topic
+from MainApp.models import Topic, Entry
 
 topics = Topic.objects.all()
+entries = Entry.objects.all()
 
 for t in topics:
     print(f"Topic ID: {t.id} and Topic Name: {t}")
 
+for e in entries:
+    print(f"Topic: {e.topic}")
+    print(f"Entry: {e.text}")
 print()
+
 t = Topic.objects.get(id=1)
 print(t.text)
 print(t.date_added)
