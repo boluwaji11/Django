13581212b9f708315f30406ed0782fd0ae4315cd
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-19 14:20:12 -0500

    forms added

--- MainApp/views.py
+++ MainApp/views.py
@@ -21,3 +21,23 @@ def topic(request, topic_id):
     context = {"topic": topic, "entries": entries}
 
     return render(request, "MainApp/topic.html", context)
+
+
+from django.shortcuts import render, redirect
+from .forms import TopicForm
+from .models import Topic
+
+
+def new_topic(request):
+    if request.method != "POST":
+        form = TopicForm()
+
+    else:
+        form = TopicForm(data=request.POST)
+
+    if form.is_valid():
+        form.save()
+        return redirect("MainApp:topics")
+
+    context = {"form": form}
+    return render(request, "MainApp/new_topic.html", context)
