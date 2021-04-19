0b94c0cf346ec49f1201737edc3c568130d5df18
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-04-19 14:05:31 -0500

    page setup completed

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # The home page for Learning Log
+    """ The home page for Learning Log """
     return render(request, "MainApp/index.html")
 
 
@@ -11,5 +11,13 @@ from .models import Topic
 
 def topics(request):
     topics = Topic.objects.order_by("date_added")
-    context = {"topic": topics}
+    context = {"topics": topics}
     return render(request, "MainApp/topics.html", context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+
+    return render(request, "MainApp/topic.html", context)
